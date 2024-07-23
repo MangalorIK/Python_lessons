1f331ec86893a4be1f9da1575cc7eb075d174052
Василий <60142740+MangalorIK@users.noreply.github.com>
2024-07-23 18:00:00 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+homework_count = 12
+hours_spent = 1.5
+course_name = "Python"
+average_time = hours_spent / homework_count
+
+print("Курс: {0}, всего задач:{1}, затрачено часов: {2}, среднее время выполнения {3} часа.".format(course_name,homework_count,hours_spent,average_time))
