3a63fcb2db3ea5966dc13e615cba8888504eed49
Василий <60142740+MangalorIK@users.noreply.github.com>
2024-08-04 09:43:42 +0300

    tuple_list

--- homework5.py
+++ homework5.py
@@ -1,5 +1,8 @@
-immutable_var = 1, 2, 'a', 'b'
+immutable_var = 1, 2, 'a', 'b', True, [1, 2, 3]
 print('Immutable tuple: ', immutable_var)
+
+# immutable_var[0] = 10
+# кортеж не поддерживает изменение элементов
 
 mutable_list = [1, 2, 'a', 'b']
 mutable_list.append("Modified")
