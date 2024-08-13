a13c3faaabd513d89784e2499fb955f98ca5e08a
Василий <60142740+MangalorIK@users.noreply.github.com>
2024-08-13 14:22:20 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,14 @@
+import fake_math
+import true_math
+
+fake_divide = fake_math.divide
+true_divide = true_math.divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
