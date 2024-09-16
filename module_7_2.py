c07b838f158174ec022ba343a3456a3a20a9f0cf
Василий <60142740+MangalorIK@users.noreply.github.com>
2024-09-16 13:41:34 +0300

    Позиционирование в файле

---
+++ module_7_2.py
@@ -0,0 +1,24 @@
+strings_positions = {}
+
+
+def custom_write(file_name, strings):
+    file = open(file_name, 'a', encoding="utf-8")
+
+    for i in range(len(strings)):
+        strings_positions[(i + 1, file.tell())] = strings[i]
+        file.write(strings[i] + "\n")
+
+    file.close()
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result:
+    print(elem)
